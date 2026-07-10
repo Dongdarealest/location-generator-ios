b87791964cf73082577e9b813a5c8e6b05d761b0
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2026-07-10 16:23:18 +0700

    Refactor MapPreview to MapManager with initialization

--- js/map.js
+++ js/map.js
@@ -1,8 +1,53 @@
-const MapPreview = {
+const MapManager = {
 
-    show(location) {
+    map: null,
 
-        // Coming in Sprint 2
+    marker: null,
+
+    initialized: false,
+
+    init() {
+
+        if (this.initialized) return;
+
+        this.map = L.map("map", {
+            zoomControl: true
+        }).setView([21.0285, 105.8542], 5);
+
+        L.tileLayer(
+            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            {
+                attribution: "&copy; OpenStreetMap contributors",
+                maxZoom: 19
+            }
+        ).addTo(this.map);
+
+        this.initialized = true;
+
+    },
+
+    update(location) {
+
+        if (!this.initialized) return;
+
+        const latlng = [location.lat, location.lon];
+
+        if (!this.marker) {
+
+            this.marker = L.marker(latlng, {
+                draggable: true
+            }).addTo(this.map);
+
+        } else {
+
+            this.marker.setLatLng(latlng);
+
+        }
+
+        this.map.flyTo(latlng, 15, {
+            animate: true,
+            duration: 1.4
+        });
 
     }
 
