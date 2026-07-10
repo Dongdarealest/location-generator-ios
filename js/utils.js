52ed7b9ceb9eefc986585b4334ed8eb11c547201
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2026-07-10 16:12:56 +0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -6,9 +6,55 @@ const Utils = {
 
     },
 
+    detectInputType(text) {
+
+        text = text.trim();
+
+        if (this.isCoordinate(text)) {
+
+            return {
+                type: "coordinate"
+            };
+
+        }
+
+        if (this.isGoogleMaps(text)) {
+
+            return {
+                type: "google"
+            };
+
+        }
+
+        if (this.isAppleMaps(text)) {
+
+            return {
+                type: "apple"
+            };
+
+        }
+
+        return {
+            type: "place"
+        };
+
+    },
+
     isCoordinate(text) {
 
         return /^-?\d+(\.\d+)?\s*,\s*-?\d+(\.\d+)?$/.test(text);
+
+    },
+
+    isGoogleMaps(text) {
+
+        return /google\.[^/]+\/maps|goo\.gl\/maps|maps\.app\.goo\.gl/i.test(text);
+
+    },
+
+    isAppleMaps(text) {
+
+        return /maps\.apple\.com/i.test(text);
 
     }
 
