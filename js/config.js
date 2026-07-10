59ef188388d4752164ab890b09fd3444d41df680
Dongdarealest <107338168+Dongdarealest@users.noreply.github.com>
2026-07-10 16:00:54 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+
+    VERSION: "1.0.0",
+
+    GEOCODER: {
+
+        URL: "https://nominatim.openstreetmap.org/search",
+
+        LIMIT: 1,
+
+        FORMAT: "jsonv2"
+
+    },
+
+    TEMPLATE: {
+
+        FILE: "templates/ios-location-spoofer.sgmodule"
+
+    }
+
+};
